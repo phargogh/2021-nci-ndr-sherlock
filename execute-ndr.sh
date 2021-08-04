8b3fdb05b249dfaae1f491f927e6e9346db88013
James Douglass <jdouglass@stanford.edu>
2021-08-04 08:29:16 -0700

    Bumping the digest to fix the detect_outlets issue, increasing cores to 20 (and double that for n_workers) and increasing time limit to 20 hours.

--- execute-ndr.sh
+++ execute-ndr.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH --job-name=NCI-NDR-plus-for-BCK-and-Rafa
 #
-#SBATCH --time=10:00:00
+#SBATCH --time=20:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=4G
 
 NDR_WORKSPACE_NAME=global_ndr_plus_workspace
@@ -32,7 +32,7 @@ then
 fi
 
 CONTAINER=ghcr.io/phargogh/inspring-no-gcloud-keys
-DIGEST=sha256:ff0fd8ea1594c35dc555273666a97d15340393772c95986097ffd826d22c0dc7
+DIGEST=sha256:66c4a760dece610f992ee2f2aa4fff6a8d9e96951bf6f9a81bf16779aa7f26c4
 
 srun singularity run docker://$CONTAINER@$DIGEST \
-	global_ndr_plus_pipeline.py scenarios.nci_global_1 --n_workers=32
+	global_ndr_plus_pipeline.py scenarios.nci_global_1 --n_workers=40
