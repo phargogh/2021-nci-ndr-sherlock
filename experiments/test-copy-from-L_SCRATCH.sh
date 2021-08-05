7ce72efab47695f21d5baf23c0209ff0fd76e69d
James Douglass <jdouglass@stanford.edu>
2021-08-05 08:41:45 -0700

    Adding a script to test copying from $L_SCRATCH.
    Does this happen before the end of the sbatch job?

---
+++ experiments/test-copy-from-L_SCRATCH.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Test Copying from $L_SCRATCH at end of job
+#
+#SBATCH --time=00:10:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+
+# Hypothesis:
+# If I'm reading the SLURM docs correctly, I should be able to finish an
+# entire sbatch script before the $L_SCRATCH partition is erased at the end of
+#the job.
+
+# Generating a 1GB file with random data in it.
+FILENAME=1GB-random-data.txt
+SOURCE_FILENAME=$L_SCRATCH/$FILENAME
+TARGET_FILENAME=$SCRATCH/$FILENAME
+srun openssl rand -out $SOURCE_FILENAME -base64 $(( 2**30 * 3/4 ))
+
+cp $SOURCE_FILENAME $TARGET_FILENAME
+
+# Conclusion:
+#
