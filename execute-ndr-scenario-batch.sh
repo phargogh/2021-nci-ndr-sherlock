cda102d93f7991efc1fdae74282f5a914547110a
James Douglass <jdouglass@stanford.edu>
2021-10-27 12:25:32 -0700

    Adding a script to sbatch individual scenarios.

---
+++ execute-ndr-scenario-batch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+CONTAINER=ghcr.io/phargogh/inspring-no-gcloud-keys
+DIGEST=sha256:66c4a760dece610f992ee2f2aa4fff6a8d9e96951bf6f9a81bf16779aa7f26c4
+
+# Fetch the repository
+REPOSLUG=ndr_plus_global_pipeline
+REPO=https://github.com/phargogh/$REPOSLUG.git
+REVISION=82c0f8b3ccc57614335eeb62c50f8261ed1ef171
+if [ ! -d $REPOSLUG ]
+then
+    git clone $REPO
+fi
+pushd $REPOSLUG
+
+# OK to always fetch the repo
+git fetch
+git checkout $REVISION
+SCENARIOS=$(python -c "import scenarios.nci_global as s; print('\n'.join(k for k in s.SCENARIOS))")
+popd
+
+for NCI_SCENARIO in $SCENARIOS
+do
+    SCENARIO_JOB_ID=$(sbatch --job-name=NCI-NDR-plus-global-rerun-Oct-2021-$NCI_SCENARIO \
+        execute-ndr-specific-scenario.sh \
+        NCI-NDRplus-$NCI_SCENARIO $NCI_SCENARIO | grep -o [0-9]\\+)
+    echo "$NCI_SCENARIO $SCENARIO_JOB_ID" >> scenario_jobs.txt
+done
---
+++ execute-ndr-specific-scenario.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#SBATCH --time=30:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=4G
+#
+# This script assumes that the task name will be set by the calling sbatch command.
+
+WORKSPACE_NAME="$1"
+SCENARIO_NAME="$2"
+
+CONTAINER=ghcr.io/phargogh/inspring-no-gcloud-keys
+DIGEST=sha256:66c4a760dece610f992ee2f2aa4fff6a8d9e96951bf6f9a81bf16779aa7f26c4
+WORKSPACE_DIR=$L_SCRATCH/$WORKSPACE_NAME
+
+mkdir -pf $WORKSPACE_DIR
+
+singularity run docker://$CONTAINER@$DIGEST \
+    --env WORKSPACE_DIR=$WORKSPACE_DIR \
+    global_ndr_plus_pipeline.py scenarios.nci_global \
+    --n_workers=40 \
+    --limit_to_scenarios $SCENARIO_NAME
+
+# copy results (regardless of job run status) to $SCRATCH
+cp -r $WORKSPACE_DIR $SCRATCH/2021-NCI-$WORKSPACE_NAME
