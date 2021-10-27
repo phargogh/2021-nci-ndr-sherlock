e3fcec31234f0b58b3a0fd5fd20f66f352264bef
James Douglass <jdouglass@stanford.edu>
2021-10-27 10:28:45 -0700

    Writing output to $L_SCRATCH, then copying to $SCRATCH.

--- experiments/test-python-env-singularity/test-echo.sh
+++ experiments/test-python-env-singularity/test-echo.sh
@@ -13,9 +13,11 @@
 # the singularity --env and assume a default if it hasn't been passed.
 
 # Expected workspace: $HOME/some-workspace
+TARGET_WORKSPACE=$L_SCRATCH/test-echo-workspace
 srun singularity run docker://python:3.9.7-bullseye \
-    --env WORKSPACE_DIR=$HOME/some-workspace \
-    echo.py
+    --env WORKSPACE_DIR=$TARGET_WORKSPACE \
+    echo.py > $TARGET_WORKSPACE/out.txt; \
+    cp -r $TARGET_WORKSPACE $SCRATCH/test-echo-workspace
 
 # Expected workspace: default/workspace
 srun singularity run docker://python:3.9.7-bullseye echo.py
