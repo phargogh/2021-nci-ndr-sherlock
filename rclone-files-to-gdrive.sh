d33f3b4765f8b00df98192832ae1b6e6f0d26450
James Douglass <jdouglass@stanford.edu>
2021-10-29 16:15:13 -0700

    Adding a sample file to batch-rclone files to gdrive.

---
+++ rclone-files-to-gdrive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=RClone-NCI-files-to-gdrive
+
+
+TARGET_DIR=2021-10-29-ndr-nci
+module load system rclone
+
+for file in `ls $SCRATCH/*NCI-NDRplus*/*.tif`
+do
+    SCENARIO=$(basename $(dirname $file) | sed 's/2021-NCI-NCI-NDRplus-//g')
+    rclone copy --progress $file "nci-ndr-stanford-gdrive:$TARGET_DIR/$SCENARIO"
+done
