f7d43c53377a1334ad9ea91a9c1cca90fa90ff16
James Douglass <jdouglass@stanford.edu>
2022-03-16 17:01:16 -0700

    Bumping the NDR revision to scrub nan values to float32 min.

--- execute-ndr-scenario-batch.sh
+++ execute-ndr-scenario-batch.sh
@@ -8,7 +8,7 @@ DIGEST=sha256:66c4a760dece610f992ee2f2aa4fff6a8d9e96951bf6f9a81bf16779aa7f26c4
 # Fetch the repository
 REPOSLUG=ndr_plus_global_pipeline
 REPO=https://github.com/phargogh/$REPOSLUG.git
-REVISION=907b5a5aa69f1522a55b836182d0b949d44a6a99
+REVISION=e7a6f10eb5b21799d5ee6571691aacdddffec0d0
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
