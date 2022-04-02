59e940f750327dc52f0c920b10902359d928a543
James Douglass <jdouglass@stanford.edu>
2022-04-02 13:19:57 -0700

    Bumping noxn revision again, for extra logging so I can better debug memory issues.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -30,7 +30,7 @@ DIGEST=sha256:6164b338bc3626e8994e2e0ffd50220fe2f66e7e904b794920749fa23360d7af
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=928a25503bd4210b22b71eaa33a8a11f28e4d0d0
+REVISION=6d7ff3173f6b6a11da723adfead832bf78ba83cb
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
