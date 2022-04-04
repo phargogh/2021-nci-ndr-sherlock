4face53ba1e98de25e938095597be17f07fedc72
James Douglass <jdouglass@stanford.edu>
2022-04-04 12:48:08 -0700

    Updating noxn revision for cleanup and resolution inputs.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -36,7 +36,7 @@ DIGEST=sha256:6164b338bc3626e8994e2e0ffd50220fe2f66e7e904b794920749fa23360d7af
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=9fdbc80b41a09c0f40a1f380c103a3b4e2f3d47f
+REVISION=526c591aab18f5c25d14df1b51a73f24c06670a6
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
