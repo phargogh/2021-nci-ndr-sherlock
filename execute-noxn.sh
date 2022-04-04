f2556bfa1960532a6e920d4f92753bb07e5363ea
James Douglass <jdouglass@stanford.edu>
2022-04-04 13:13:32 -0700

    Turning memory needs down to 4GB.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=1:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jdouglass@stanford.edu
 #SBATCH --partition=hns,normal
