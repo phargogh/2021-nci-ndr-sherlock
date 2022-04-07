e311720e114ada7cd85a320cca6f950781defea6
James Douglass <jdouglass@stanford.edu>
2022-04-07 13:41:56 -0700

    Adding a few comments.

--- upload-to-googledrive.sh
+++ upload-to-googledrive.sh
@@ -15,10 +15,15 @@ DEST_DIR="$1"
 # Loop through all of the remaining args and assume they are files.
 module load system rclone
 rclone mkdir "$DEST_DIR"
+
 for file in "${@:2}"
 do
     rclone copy --progress "$file" "$DEST_DIR" &
 done
+
+# This strategy can be used instead if it's important to limit the number of
+# uploads we're using.  So far, I haven't found that this is strictly needed.
+# echo ${@:2} | tr " " "\n" | parallel -j5 rclone copy --progress {} "$DEST_DIR"
 
 wait
 echo 'done!'
