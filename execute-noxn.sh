85dfbbf3fa5ba66aaa7caa9105879bd0b57f916d
James Douglass <jdouglass@stanford.edu>
2022-05-04 15:29:30 -0700

    Bumping down the number of worker processes in NOXN.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --time=2:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jdouglass@stanford.edu
