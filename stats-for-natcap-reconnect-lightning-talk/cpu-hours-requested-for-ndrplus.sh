71198fcee98ce55d4ed79398445bbbfce1aa3544
James Douglass <jdouglass@stanford.edu>
2022-05-17 09:41:35 -0700

    Actually running sacct.

--- stats-for-natcap-reconnect-lightning-talk/cpu-hours-requested-for-ndrplus.sh
+++ stats-for-natcap-reconnect-lightning-talk/cpu-hours-requested-for-ndrplus.sh
@@ -4,6 +4,6 @@ for yyyy in {2021..2022}
 do
     for mm in {01..12}
     do
-        echo "sacct -u jadoug06 --starttime \"$yyyy-$mm-01\" --endtime \"$yyyy-$mm-31\" | grep NDR"
+        sacct -u jadoug06 --starttime \"$yyyy-$mm-01\" --endtime \"$yyyy-$mm-31\" | grep NDR
     done
 done
