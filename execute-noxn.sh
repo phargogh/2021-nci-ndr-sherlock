e60bd03d45c99763717f9e6500f19d38eaae544a
James Douglass <jdouglass@stanford.edu>
2022-06-27 21:11:08 -0700

    Bumping the noxn revision to improve charting.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -36,7 +36,7 @@ DIGEST=sha256:6164b338bc3626e8994e2e0ffd50220fe2f66e7e904b794920749fa23360d7af
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=8daf15a02d25d6842b9c826caf4ada7bf1b63e35
+REVISION=8c0d4c42fb73454eac2ec70f6333f21b9e63f2e9
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
