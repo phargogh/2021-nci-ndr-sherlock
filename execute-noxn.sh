9b19cf239489925593f21f15e2bfa47a675b6f6e
James Douglass <jdouglass@stanford.edu>
2022-06-28 15:05:10 -0700

    Bumping noxn revision to fix a minor issue in my task graph.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -36,7 +36,7 @@ DIGEST=sha256:6164b338bc3626e8994e2e0ffd50220fe2f66e7e904b794920749fa23360d7af
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=2af5b4d1059de887f7bcd7e23d806a6a8d4ee5fa
+REVISION=31c93cbbfac88fcc0b4725902c57b4c8715373b6
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
