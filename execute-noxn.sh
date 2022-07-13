dcbfd8def306d84123646fa54bd0abca39e071c0
James Douglass <jdouglass@stanford.edu>
2022-07-13 13:44:00 -0700

    Bumping noxn revision to add Rafa's all_natural' scenario.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -36,7 +36,7 @@ DIGEST=sha256:a9e09ff873407ce9e315504b019c616bf59095d65dcff4f31e1d4886722c8b46
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=4dbde364aebe8dbc55b34a4ccbdcaef7147bf31f
+REVISION=bcdcd303b6b469e56f0551f10c5002f8f2a396b3
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
