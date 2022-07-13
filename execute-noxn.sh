da1c7f9ead255b868cbe7cf2b63eeff7c6ddfead
James Douglass <jdouglass@stanford.edu>
2022-07-13 14:55:44 -0700

    Bumping noxn revision to update the ALLNATURAL scenario's export scenario from baseline to restoration.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -36,7 +36,7 @@ DIGEST=sha256:a9e09ff873407ce9e315504b019c616bf59095d65dcff4f31e1d4886722c8b46
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=c7847cbcb03d20d9a6cd645a9bcb92051c80ab2f
+REVISION=f266b430bf740b21b05b22fa7883b8369b7c6353
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
