da7b604703f969c7b5d3eab075bc68901cecf37f
James Douglass <jdouglass@stanford.edu>
2022-08-03 17:07:44 -0700

    Bumping the noxn revision for BCK and Rafa's new 'baseline' scenario.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -36,7 +36,7 @@ DIGEST=sha256:a9e09ff873407ce9e315504b019c616bf59095d65dcff4f31e1d4886722c8b46
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=d2adf54a34d1b7fefc554eadd21efe8c6af24ebc
+REVISION=8de820f34ce2218f13ef6200617d1531988c1d63
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
