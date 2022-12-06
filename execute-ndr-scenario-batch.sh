beb6ac4bfce8ebd64c8651d3afd31a727bb1fea7
James Douglass <jdouglass@stanford.edu>
2022-12-06 15:51:43 -0800

    Updating NDR scenario batch script.
    This adds in the two raster preprocessing scripts (for lulc
    preprocessing and N application rasters) and makes them dependencies of
    the NDR scenario runs.

--- execute-ndr-scenario-batch.sh
+++ execute-ndr-scenario-batch.sh
@@ -9,13 +9,26 @@ NDR_REVISION=0e9dd91aefd2dfd2bdac143de9b5aca682c5e123
 
 echo "***********************************************************************"
 echo "Beginning NDR Batch"
+echo "Including: "
+echo "   LULC script processing"
+echo "   N Application processing"
+echo "   NDR scenarios"
+echo "   Water Quality"
 echo "Started $DATE"
 echo "Sherlock repo rev: $GIT_REV"
 echo "NDR repo rev: $NDR_REVISION"
 echo "Extra args: $1 $2"
 echo "***********************************************************************"
 
+# - [X] sbatch the 2 scenario tasks
+# - [X] make NDR scenarios dependent on the 2 scenario tasks
+# - [ ] override the job name in the 2 scenario tasks
+# - [ ] write the job scenario tasks to a single workspace
+
+
 set -x
+LULC_SCENARIOS_JOB=$(sbatch --job-name="NCI-WQ-LULC-scenarios-$GIT_REV" build-ndr-scenarios.sh)
+N_APP_SCENARIOS_JOB=$(sbatch --job-name="NCI-WQ-N-application-$GIT_REV" build-n-app-scenarios.sh)
 
 # Fetch the repository
 if [ ! -d $REPOSLUG ]
@@ -35,6 +48,9 @@ module load python/3.9.0
 # the container.
 module unload numpy
 
+# We only need the scenario names, not the keys.  We won't know all the files
+# that are used in the scenarios until the `build-ndr-scenarios.sh` and
+# `build-n-app-scenarios.sh` tasks finish.
 SCENARIOS=$(python3 -c "import scenarios.nci_global_sept_2022_scenario_redesign as s; print('\n'.join(k for k in s.SCENARIOS))")
 popd
 
@@ -42,7 +58,8 @@ popd
 # we're not supposed to call sbatch from within a loop.  A loop is the only way
 # this makes sense, though, and it isn't usually very many.  I've added a sleep
 # to help avoid a possible denial-of-service.
-SLURM_DEPENDENCY_STRING="afterok"
+NDR_SLURM_DEPENDENCY_STRING="afterok:$LULC_SCENARIOS_JOB:$N_APP_SCENARIOS_JOB"
+NOXN_SLURM_DEPENDENCY_STRING="afterok:"
 for NCI_SCENARIO in $SCENARIOS
 do
     # Can also redirect stdout/stderr if needed:
@@ -51,11 +68,12 @@ do
 
     WORKSPACE_DIR=NCI-NDRplus-$GIT_REV-$NCI_SCENARIO
     SCENARIO_JOB_ID=$(sbatch \
-        --job-name="NCI-NDRplus-$NCI_SCENARIO-global-$DATE" \
+        --job-name="NCI-NDR-$NCI_SCENARIO-$DATE" \
         --chdir=$REPOSLUG \
+        --dependency="$NDR_SLURM_DEPENDENCY_STRING" \
         execute-ndr-specific-scenario.sh \
         "$WORKSPACE_DIR" "$NCI_SCENARIO" "$DATE" "$GIT_REV" | grep -o [0-9]\\+)
-    SLURM_DEPENDENCY_STRING="$SLURM_DEPENDENCY_STRING:$SCENARIO_JOB_ID"
+    NOXN_SLURM_DEPENDENCY_STRING="$NOXN_SLURM_DEPENDENCY_STRING:$SCENARIO_JOB_ID"
     echo "$NCI_SCENARIO $SCENARIO_JOB_ID" >> scenario_jobs.txt
 
     # Give slurmctld a break for 2s just to be safe, in case they try to deny
@@ -69,6 +87,6 @@ then
     # --dependency=afterok:<id1>:<id2>... means that if the whole NDR pipeline
     # passes, then we'll trigger the NOXN pipeline.
     sbatch \
-        --dependency="$SLURM_DEPENDENCY_STRING" \
+        --dependency="$NOXN_SLURM_DEPENDENCY_STRING" \
         ./execute-noxn.sh "$2" "$GIT_REV"
 fi
