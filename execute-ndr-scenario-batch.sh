bc2128226ece0da327667ae3770501bef592b957
James Douglass <jdouglass@stanford.edu>
2022-12-06 19:30:17 -0800

    Reworking ndr batch script to include scenario file linting.

--- execute-ndr-scenario-batch.sh
+++ execute-ndr-scenario-batch.sh
@@ -5,7 +5,7 @@ DATE="$(date +%F)"
 GIT_REV="rev$(git rev-parse --short HEAD)"
 REPOSLUG=ndr_plus_global_pipeline
 REPO=https://github.com/phargogh/$REPOSLUG.git
-NDR_REVISION=0e9dd91aefd2dfd2bdac143de9b5aca682c5e123
+NDR_REVISION=ec3d0f5e547985720ee3b3dd388382ace56a1a6d
 
 echo "***********************************************************************"
 echo "Beginning NDR Batch"
@@ -25,7 +25,8 @@ echo "***********************************************************************"
 # - [X] override the job name in the 2 scenario tasks
 # - [X] write the job scenario tasks to a single workspace
 # - [ ] assert that no nan or inf values are written to rasters.
-# - [ ] upload all target files to a consistent place on google drive
+# - [X] upload all target files to a consistent place on google drive
+# - [ ] rewrite the ndr scenario to load rasters from json files (and lint before starting big jobs)
 
 
 set -x
@@ -35,18 +36,31 @@ FULL_WQ_PIPELINE_WORKSPACE="$SCRATCH/NCI-WQ-full-${DATE}-${GIT_REV}"
 rm -r "$FULL_WQ_PIPELINE_WORKSPACE" || echo "Cannot remove directory that isn't there."
 mkdir -p "$FULL_WQ_PIPELINE_WORKSPACE" || echo "Cannot create directory that exists."
 
+LULC_SCENARIOS_WORKSPACE="$FULL_WQ_PIPELINE_WORKSPACE/NCI-WQ-inputs-lulc-scenarios"
 LULC_SCENARIOS_JOB=$(sbatch \
     --job-name="NCI-WQ-LULC-scenarios-$GIT_REV" \
     build-ndr-scenarios.sh \
     "$LOCAL_GDRIVE_INPUTS_DIR" \
-    "$FULL_WQ_PIPELINE_WORKSPACE/NCI-WQ-inputs-lulc-scenarios" \
+    "$LULC_SCENARIOS_WORKSPACE" \
     "$FULL_WQ_PIPELINE_WORKSPACE" | grep -o "[0-9]\\+")
+
+N_APP_SCENARIOS_WORKSPACE="$FULL_WQ_PIPELINE_WORKSPACE/NCI-WQ-inputs-n-application"
 N_APP_SCENARIOS_JOB=$(sbatch \
     --job-name="NCI-WQ-N-application-$GIT_REV" \
     build-n-app-scenarios.sh \
     "$LOCAL_GDRIVE_INPUTS_DIR" \
-    "$FULL_WQ_PIPELINE_WORKSPACE/NCI-WQ-inputs-n-application" \
+    "$N_APP_SCENARIOS_WORKSPACE" \
     "$FULL_WQ_PIPELINE_WORKSPACE" | grep -o "[0-9]\\+")
+
+# Verify that our rasters at least exist and the json has the right keys.
+# It's costly (in terms of job priority) to have lots of big jobs submitted and
+# started, only to have them fail for silly reasons like filepath/key mismatches.
+RASTER_LINTING_JOB=$(sbatch \
+    --job-name="NCI-WQ-lint-preprocessed-files-$GIT_REV" \
+    --dependency="afterok:$LULC_SCENARIOS_JOB:$N_APP_SCENARIOS_JOB" \
+    lint-ndr-scenario.sh \
+    "$LULC_SCENARIOS_WORKSPACE/Scenarios_ecosharded/scenarios.json" \
+    "$N_APP_SCENARIOS_WORKSPACE/N_application/rasters.json" | grep "[0-9]\\+")
 
 # Fetch the repository
 if [ ! -d $REPOSLUG ]
@@ -76,7 +90,7 @@ popd
 # we're not supposed to call sbatch from within a loop.  A loop is the only way
 # this makes sense, though, and it isn't usually very many.  I've added a sleep
 # to help avoid a possible denial-of-service.
-NDR_SLURM_DEPENDENCY_STRING="afterok:$LULC_SCENARIOS_JOB:$N_APP_SCENARIOS_JOB"
+NDR_SLURM_DEPENDENCY_STRING="afterok:$RASTER_LINTING_JOB"
 NOXN_SLURM_DEPENDENCY_STRING="afterok:"
 for NCI_SCENARIO in $SCENARIOS
 do
