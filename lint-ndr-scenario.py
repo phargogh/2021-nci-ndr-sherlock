26f5cec17a4709e09ef26d7fd0c465f79c08c153
James Douglass <jdouglass@stanford.edu>
2022-12-06 19:45:50 -0800

    Fixing exception handling for ecoshard keys

--- lint-ndr-scenario.py
+++ lint-ndr-scenario.py
@@ -13,16 +13,18 @@ def main(scenario_name):
     error = False
     for ndr_scenario_key, files_dict in scenario.SCENARIOS.items():
         for file_key, ecoshards_key in files_dict.items():
-            file_path = scenario.ECOSHARDS[ecoshards_key]
+            try:
+                file_path = scenario.ECOSHARDS[ecoshards_key]
+            except KeyError:
+                LOGGER.error(
+                    "Key not found in ECOSHARDS: "
+                    f"{ndr_scenario_key}:{file_key}")
+                error = True
+
             if not os.path.exists(file_path):
                 LOGGER.error(
                     "File not found: "
                     f"{ndr_scenario_key}:{file_key}:{file_path}")
-                error = True
-            if file_key not in scenario.ECOSHARDS:
-                LOGGER.error(
-                    "Key not found in ECOSHARDS: "
-                    f"{ndr_scenario_key}:{file_key}")
                 error = True
 
     if error:
