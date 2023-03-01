4dd36aecd73ea41a23bb83f771f0b021c967fb49
James Douglass <jdouglass@stanford.edu>
2023-02-28 16:13:32 -0800

    Converting data copy script to use globus.

--- copy-workspace-to-oak.sh
+++ copy-workspace-to-oak.sh
@@ -10,16 +10,19 @@
 #SBATCH --job-name="NCI-copy-workspace-to-oak"
 #SBATCH --output=/scratch/users/jadoug06/slurm-logfiles/slurm-%j.%x.out
 
-SOURCE_NCI_WQ_WORKSPACE="$1"
+SOURCE_NCI_WQ_WORKSPACE=$(cat $1 | sed "s|$SCRATCH||g")
 BASENAME=$(basename "$1")
 
 TARGET_PARENT_DIR="$OAK/nci/wq-latest"
 
-# We only want to keep the latest workspace around, so just delete the prior one
-rm -r "${TARGET_PARENT_DIR:?}/*" || echo "Directory not found; skipping: $TARGET_PARENT_DIR"
+# These are obtained by `globus bookmark list` from JD's globus bookmarks
+GLOBUS_OAK_ENDPOINT=8b3a8b64-d4ab-4551-b37e-ca0092f769a7
+GLOBUS_SCRATCH_ENDPOINT=6881ae2e-db26-11e5-9772-22000b9da45e
 
-mkdir -p "$TARGET_PARENT_DIR" || echo "Directory already exists: $TARGET_PARENT_DIR"
-cp -rv "$SOURCE_NCI_WQ_WORKSPACE" "$TARGET_PARENT_DIR/$BASENAME"
+# We only want to keep the latest workspace around, so just delete the prior one.
+# This globus call is blocking.
+globus rm --ignore-missing "$GLOBUS_OAK_ENDPOINT:/nci/wq-latest/*"
 
-echo "Copy complete!"
-echo "View the outputs at $TARGET_PARENT_DIR/$BASENAME"
+globus transfer "$GLOBUS_SCRATCH_ENDPOINT:$SOURCE_NCI_WQ_WORKSPACE" "$GLOBUS_OAK_ENDPOINT:/nci/wq-latest/$BASENAME"
+
+echo "Transfer request submitted for $TARGET_PARENT_DIR/$BASENAME"
