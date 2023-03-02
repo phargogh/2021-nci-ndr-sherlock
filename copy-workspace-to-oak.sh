5415f4b88501415c4d0e929cf37f32cef6682652
James Douglass <jdouglass@stanford.edu>
2023-03-02 11:54:24 -0800

    Using local rm instead of globus directory deletion.
    Globus directory deletion took WAY too long - the job timed out.  By
    contrast, rm -r took care of the directory in a few seconds.

--- copy-workspace-to-oak.sh
+++ copy-workspace-to-oak.sh
@@ -15,16 +15,18 @@ set -x
 SOURCE_NCI_WQ_WORKSPACE="${1//$SCRATCH/}"  # replace $SCRATCH with empty string
 BASENAME=$(basename "$1")
 
-TARGET_PARENT_DIR="$OAK/nci/wq-latest"
+TARGET_PARENT_DIR="nci/wq-latest"
+OAK_TARGET_PARENT_DIR="$OAK/$TARGET_PARENT_DIR"
 
 # These are obtained by `globus bookmark list` from JD's globus bookmarks
 GLOBUS_OAK_ENDPOINT="8b3a8b64-d4ab-4551-b37e-ca0092f769a7"
 GLOBUS_SCRATCH_ENDPOINT="6881ae2e-db26-11e5-9772-22000b9da45e"
 
 # We only want to keep the latest workspace around, so just delete the prior one.
-# This globus call is blocking.
-globus rm --ignore-missing --heartbeat "$GLOBUS_OAK_ENDPOINT:/nci/wq-latest/*"
+# rm -r is super fast compared with using globus to delete the directory.
+# There's so much to delete that the globus job will time out!
+rm -r "${OAK_TARGET_PARENT_DIR:?}/*" || echo "Could not remove $OAK_TARGET_PARENT_DIR"
 
-globus transfer "$GLOBUS_SCRATCH_ENDPOINT:$SOURCE_NCI_WQ_WORKSPACE" "$GLOBUS_OAK_ENDPOINT:/nci/wq-latest/$BASENAME"
+globus transfer "$GLOBUS_SCRATCH_ENDPOINT:$SOURCE_NCI_WQ_WORKSPACE" "$GLOBUS_OAK_ENDPOINT:/$TARGET_PARENT_DIR/$BASENAME"
 
-echo "Transfer request submitted for $TARGET_PARENT_DIR/$BASENAME"
+echo "Transfer request submitted for $OAK_TARGET_PARENT_DIR/$BASENAME"
