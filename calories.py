3fed558517fc84342dd4193cb33d759573e71dd2
James Douglass <jdouglass@stanford.edu>
2023-04-07 16:25:35 -0700

    Building out calories pipeline - normal warping.

--- calories.py
+++ calories.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 import shutil
@@ -9,10 +10,12 @@ import numpy
 import pygeoprocessing
 from osgeo import gdal
 from osgeo import osr
+from pygeoprocessing import geoprocessing
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 logging.getLogger('pygeoprocessing').setLevel(logging.DEBUG)
+logging.getLogger('taskgraph').setLevel(logging.DEBUG)
 
 # Maps lucode to the proportion of calories produced.
 AG_INTENSITY = {
@@ -27,15 +30,17 @@ AG_INTENSITY = {
     30: 0.75,  # Mosaic cropland (>50%)
     40: 0.25,  # Mosaic cropland (<50%)
 }
-AG_CURRENT = {10, 11, 12, 20}
-AG_IRRIGATED = {15, 25, 26}
-AG_RAINFED = {16}
+AG_CURRENT = {10, 11, 12, 20, 30, 40}
+AG_INTENSIFIED_IRRIGATED = {15, 25, 26}
+AG_INTENSIFIED_RAINFED = {16}
 assert (
-    len(AG_CURRENT.intersection(AG_IRRIGATED).intersection(AG_RAINFED)) == 0)
+    len(AG_CURRENT.intersection(AG_INTENSIFIED_IRRIGATED).intersection(
+        AG_INTENSIFIED_RAINFED)) == 0)
 assert (
-    AG_CURRENT.union(AG_IRRIGATED).union(AG_RAINFED) ==
-    set(AG_INTENSITY.keys()))
+    AG_CURRENT.union(AG_INTENSIFIED_IRRIGATED).union(AG_INTENSIFIED_RAINFED)
+    == set(AG_INTENSITY.keys()))
 TARGET_NODATA = float(numpy.finfo(numpy.float32).min)
+FLOAT32_NODATA = TARGET_NODATA
 
 
 def calories(lulc_raster_path,
@@ -77,60 +82,120 @@ def calories(lulc_raster_path,
         # on the case.
         for ag_lucode, ag_proportion in AG_INTENSITY.items():
             this_class = (lulc == ag_lucode)
-            if ag_lucode in AG_IRRIGATED:
+            if ag_lucode in AG_INTENSIFIED_IRRIGATED:
                 result[this_class] = irrigated_calories[this_class]
-            elif ag_lucode in AG_RAINFED:
+            elif ag_lucode in AG_INTENSIFIED_RAINFED:
                 result[this_class] = rainfed_calories[this_class]
             else:  # Use the current calories
                 result[this_class] = current_calories[this_class]
-            result[this_class] *= (ag_proportion * scalar)
+            result[this_class] *= ag_proportion
 
         return result
 
     pygeoprocessing.raster_calculator(
-        [(lulc_raster_path, 1), (base_calories_raster_path, 1)],
+        [(lulc_raster_path, 1),
+         (current_calories_raster_path, 1),
+         (irrigated_calories_raster_path, 1),
+         (rainfed_calories_raster_path, 1)],
         _get_calories, target_raster_path, gdal.GDT_Float32, TARGET_NODATA,
         use_shared_memory=True)
 
 
-def main_vrt():
-    lulc_path = sys.argv[1]
-    base_calories_path = sys.argv[2]
+def _log_rmtree_error(function, path, excinfo):
+    LOGGER.warning(
+        f'{function}() could not remove path {path}: {excinfo}')
 
-    temp_dir = tempfile.mkdtemp(dir=os.getcwd(), prefix='aligned-calories-')
-    temp_calories = os.path.join(temp_dir, "calories_aligned.vrt")
-    LOGGER.info(f"Building temp calories raster at {temp_calories}")
 
-    lulc_info = pygeoprocessing.get_raster_info(lulc_path)
-    target_srs = osr.SpatialReference()
-    target_srs.ImportFromWkt(lulc_info['projection_wkt'])
+def _convert_to_from_density(source_raster_path, target_raster_path,
+                             direction='to_density'):
+    """Convert a raster to/from counts/pixel and counts/unit area.
 
-    times = []
-    start_time = time.time()
-    gdal.BuildVRT(
-        temp_calories,
-        [base_calories_path],
-        outputBounds=lulc_info['bounding_box'],
-        xRes=abs(lulc_info['pixel_size'][0]),
-        yRes=abs(lulc_info['pixel_size'][1]),
-        allowProjectionDifference=True,
-        resampleAlg='near',
-        VRTNodata=TARGET_NODATA,
-        outputSRS=target_srs)
-    times.append(time.time() - start_time)
+    Args:
+        source_raster_path (string): The path to a raster containing units that
+            need to be converted.
+        target_raster_path (string): The path to where the target raster with
+            converted units should be stored.
+        direction='to_density' (string): The direction of unit conversion.  If
+            'to_density', then the units of ``source_raster_path`` must be in
+            counts per pixel and will be converted to counts per square meter.
+            If 'from_density', then the units of ``source_raster_path`` must be
+            in counts per square meter and will be converted to counts per
+            pixel.
 
-    start_time = time.time()
-    calories(lulc_path, temp_calories, 'calories.tif', scalar=1)
-    times.append(time.time() - start_time)
-    shutil.rmtree(temp_dir, ignore_errors=True)
-    return times
+    Returns:
+        ``None``
+    """
+    LOGGER.info(f'Converting {direction} {source_raster_path} --> '
+                f'{target_raster_path}')
+    source_raster_info = pygeoprocessing.get_raster_info(source_raster_path)
+    source_nodata = source_raster_info['nodata'][0]
 
+    # Calculate the per-pixel area based on the latitude.
+    _, miny, _, maxy = source_raster_info['bounding_box']
+    pixel_area_in_m2_by_latitude = (
+        geoprocessing._create_latitude_m2_area_column(
+            miny, maxy, source_raster_info['raster_size'][1]))
+
+    def _convert(array, pixel_area):
+        out_array = numpy.full(array.shape, FLOAT32_NODATA,
+                               dtype=numpy.float32)
+        valid_mask = slice(None)
+        if source_nodata is not None:
+            valid_mask = ~numpy.isclose(array, source_nodata)
+
+        if direction == 'to_density':
+            out_array[valid_mask] = array[valid_mask] / pixel_area[valid_mask]
+        elif direction == 'from_density':
+            out_array[valid_mask] = array[valid_mask] * pixel_area[valid_mask]
+        else:
+            raise AssertionError(f'Invalid direction: {direction}')
+        return out_array
+
+    pygeoprocessing.raster_calculator(
+        [(source_raster_path, 1), pixel_area_in_m2_by_latitude],
+        _convert, target_raster_path, gdal.GDT_Float32, FLOAT32_NODATA)
+
+
+def _align_pixel_counts_covariate_raster(
+        source_covariate_raster_path, target_covariate_raster_path,
+        target_pixel_size, target_bounding_box, target_srs_wkt, working_dir):
+    temp_dir = tempfile.mkdtemp(dir=working_dir, prefix='align-covariate-')
+
+    density_filepath = os.path.join(temp_dir, 'units_per_sq_m.tif')
+    _convert_to_from_density(
+        source_covariate_raster_path, density_filepath, 'to_density')
+
+    warped_filepath = os.path.join(temp_dir, 'aligned_per_sq_m.tif')
+    pygeoprocessing.geoprocessing.warp_raster(
+        density_filepath, target_pixel_size, warped_filepath, 'bilinear',
+        target_bounding_box, target_projection_wkt=target_srs_wkt,
+        working_dir=temp_dir)
+
+    _convert_to_from_density(
+        warped_filepath, target_covariate_raster_path, 'from_density')
+
+    shutil.rmtree(temp_dir, onerror=_log_rmtree_error)
+
+
+def calories_pipeline(
+        scenario_lulc_raster_list,
+        source_calories_raster_list,
+        target_workspace, n_workers=-1):
+    pass
+
+
+
+
+
+
+
+CALORIELAYERS = '/Users/jdouglass/Downloads/April21_2022_CalorieLayers'
 
 if __name__ == '__main__':
-    start_time = time.time()
-    vrt_times_array = main_vrt()
-    vrt_time = time.time() - start_time
-
-    print(f"VRT time:  {vrt_time}")
-    print(f"  * VRT:  {vrt_times_array[0]}")
-    print(f"  * Calc: {vrt_times_array[1]}")
+    lulc = 'prep-ndr-inputs-workspace/intensification.tif'
+    calories_layers = {
+        'current': os.path.join(CALORIELAYERS, 'caloriemapscurrentRevQ.tif'),
+        'irrigated': os.path.join(
+            CALORIELAYERS, 'caloriemapsirrigatedRevQ.tif'),
+        'rainfed': os.path.join(CALORIELAYERS, 'caloriemapsrainfedRevQ.tif'),
+    }
