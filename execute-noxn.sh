1b3cad497ff9bf71471bc4ba90e4ba4c039508af
James Douglass <jdouglass@stanford.edu>
2023-04-10 15:42:57 -0700

    Bumping noxn rev for updates to calories.
    Also:
     * removing percentiles dirs after completion
     * trying a different matplotlib interpolation scheme

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -45,7 +45,7 @@ DIGEST=sha256:74f434bd743e834120604e6a73a144260c953df5b7bcb23b8ea677346ca037a3
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=269510026931792db236d7b77b5c97567c80fff7
+REVISION=a362f42f4bed046eaff73891b4a3f95ecebe89ea
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
