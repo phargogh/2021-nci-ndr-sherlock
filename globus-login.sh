047031aa61ec6db29042c45efc42716c92b59ac5
James Douglass <jdouglass@stanford.edu>
2023-04-11 08:32:10 -0700

    Various updates to the globus login script.

--- globus-login.sh
+++ globus-login.sh
@@ -11,11 +11,20 @@ globus login  # exit code 4 if not logged in
 echo "Checking access to Globus:Oak"
 globus collection show "$GLOBUS_OAK_COLLECTION_ID" || globus login --gcs "$GLOBUS_OAK_COLLECTION_ID"  # exit code 4 if not logged in
 
+echo "Checking scopes on Globus:Oak"
+globus ls $GLOBUS_OAK_COLLECTION_ID --filter=abcd1234
+
 echo "Checking access to Globus:Scratch"
-globus endpoint show "$GLOBUS_SHERLOCK_SCRATCH_ENDPOINT_ID" || login "$GLOBUS_SHERLOCK_SCRATCH_ENDPOINT_ID"  # exit code 4 if not logged in (automatically logged in if on Sherlock)
+globus endpoint show "$GLOBUS_SHERLOCK_SCRATCH_ENDPOINT_ID" || globus login "$GLOBUS_SHERLOCK_SCRATCH_ENDPOINT_ID"  # exit code 4 if not logged in (automatically logged in if on Sherlock)
+
+echo "Checking scopes on Globus:Oak"
+globus ls $GLOBUS_SHERLOCK_SCRATCH_ENDPOINT_ID --filter=abcd1234
 
 echo "Checking access to Globus:Stanford GDrive"
-globus collection show "$GLOBUS_STANFORD_GDRIVE_COLLECTION_ID"  || login --gcs "$GLOBUS_STANFORD_GDRIVE_COLLECTION_ID"  # exit code 4 if not logged in
+globus collection show "$GLOBUS_STANFORD_GDRIVE_COLLECTION_ID"  || globus login --gcs "$GLOBUS_STANFORD_GDRIVE_COLLECTION_ID"  # exit code 4 if not logged in
+
+echo "Checking scopes on Globus:Stanford GDrive"
+globus ls $GLOBUS_STANFORD_GDRIVE_COLLECTION_ID --filter=abcd1234
 
 # If the script completed, then everything should be fine for copying files around later.
 echo "Looks like everything is set up to copy files around for NCI"
