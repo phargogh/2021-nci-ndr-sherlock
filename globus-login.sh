04ed12d0325b0bd1bbbb1f6bc185cbd21b329c52
James Douglass <jdouglass@stanford.edu>
2023-04-10 18:48:44 -0700

    Using globus login directly.

--- globus-login.sh
+++ globus-login.sh
@@ -5,10 +5,10 @@ set -e  # fail on nonzero exit code
 # load endpoint IDs, shared across scripts
 source globus-endpoints.env
 
-globus whoami  # exit code 4 if not logged in
-globus collection show "$GLOBUS_OAK_COLLECTION_ID"  # exit code 4 if not logged in
-globus endpoint show "$GLOBUS_SHERLOCK_SCRATCH_ENDPOINT_ID"  # exit code 4 if not logged in (automatically logged in if on Sherlock)
-globus collection show "$GLOBUS_STANFORD_GDRIVE_COLLECTION_ID"  # exit code 4 if not logged in
+globus login  # exit code 4 if not logged in
+globus login --gcs "$GLOBUS_OAK_COLLECTION_ID"  # exit code 4 if not logged in
+globus login "$GLOBUS_SHERLOCK_SCRATCH_ENDPOINT_ID"  # exit code 4 if not logged in (automatically logged in if on Sherlock)
+globus login --gcs "$GLOBUS_STANFORD_GDRIVE_COLLECTION_ID"  # exit code 4 if not logged in
 
 # If the script completed, then everything should be fine for copying files around later.
 echo "Looks like everything is set up to copy files around for NCI"
