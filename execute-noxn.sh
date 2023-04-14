683f1785dd5d91d7681cb1cc7c496f4fa3222c9d
James Douglass <jdouglass@stanford.edu>
2023-04-13 21:05:08 -0700

    Upping the RAM available for noxn pipeline per SRCC recommendation.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=2:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=6G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jdouglass@stanford.edu
 #SBATCH --partition=hns,normal
