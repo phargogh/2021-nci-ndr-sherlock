c7d33e90705eb7accbcc91a87f0da781cd05d139
James Douglass <jdouglass@stanford.edu>
2023-04-14 12:09:55 -0700

    Opening memmap in writeable mode

--- experiments/test-numpy-memmap-in-matplotlib/test-numpy-memmap.py
+++ experiments/test-numpy-memmap-in-matplotlib/test-numpy-memmap.py
@@ -7,7 +7,7 @@ from osgeo import gdal
 gdal.SetCacheMax(2**30)  # about 1.1 GB
 
 memmap_path = f'{os.environ["SCRATCH"]}/memmap.npym'
-memmapped_array = numpy.memmap(memmap_path, dtype=numpy.float32)
+memmapped_array = numpy.memmap(memmap_path, dtype=numpy.float32, mode='w+')
 
 raster_path = f'{os.environ["SCRATCH"]}/NCI-WQ-full-2023-04-11-rev443-047031a/NCI-NDRplus-rev443-047031a-intensification/compressed_intensification_300.0_D8_export.tif'
 raster = gdal.OpenEx(raster_path)
