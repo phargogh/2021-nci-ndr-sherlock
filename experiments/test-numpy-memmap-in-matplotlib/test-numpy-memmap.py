0210136660409edc83ee2d3f820e5a9516f6f50f
James Douglass <jdouglass@stanford.edu>
2023-04-14 13:47:29 -0700

    Loading existing memmapped file if we can.

--- experiments/test-numpy-memmap-in-matplotlib/test-numpy-memmap.py
+++ experiments/test-numpy-memmap-in-matplotlib/test-numpy-memmap.py
@@ -14,15 +14,21 @@ raster_path = f'{os.environ["SCRATCH"]}/NCI-WQ-full-2023-04-11-rev443-047031a/NC
 raster_info = pygeoprocessing.get_raster_info(raster_path)
 memmap_path = f'{os.environ["SCRATCH"]}/memmap.npym'
 LOGGER.info("Creating memmapped array")
-memmapped_array = numpy.memmap(
-    memmap_path, dtype=numpy.float32, mode='w+', shape=(
-        raster_info['raster_size'][1], raster_info['raster_size'][0]))
 
-LOGGER.info("Opening raster")
-raster = gdal.OpenEx(raster_path)
-band = raster.GetRasterBand(1)
-LOGGER.info("Loading memmapped array")
-array = band.ReadAsArray(buf_obj=memmapped_array)
+# Only load the file if it doesn't already exist -- takes some time
+if not os.path.exists(memmap_path):
+    memmapped_array = numpy.memmap(
+        memmap_path, dtype=numpy.float32, mode='w+', shape=(
+            raster_info['raster_size'][1], raster_info['raster_size'][0]))
+    LOGGER.info("Opening raster")
+    raster = gdal.OpenEx(raster_path)
+    band = raster.GetRasterBand(1)
+    LOGGER.info("Loading memmapped array")
+    array = band.ReadAsArray(buf_obj=memmapped_array)
+else:
+    array = numpy.memmap(
+        memmap_path, dtype=numpy.float32, mode='r+', shape=(
+            raster_info['raster_size'][1], raster_info['raster_size'][0]))
 
 LOGGER.info("Writing the array to the image")
 plt.imshow(array)
