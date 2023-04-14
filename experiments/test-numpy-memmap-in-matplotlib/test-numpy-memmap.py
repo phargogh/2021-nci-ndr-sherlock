103c132b2ee2ebef9561d75995f5ed4c88a4f4c7
James Douglass <jdouglass@stanford.edu>
2023-04-14 12:19:20 -0700

    Specifying the raster shape.

--- experiments/test-numpy-memmap-in-matplotlib/test-numpy-memmap.py
+++ experiments/test-numpy-memmap-in-matplotlib/test-numpy-memmap.py
@@ -2,14 +2,18 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy
+import pygeoprocessing
 from osgeo import gdal
 
 gdal.SetCacheMax(2**30)  # about 1.1 GB
 
-memmap_path = f'{os.environ["SCRATCH"]}/memmap.npym'
-memmapped_array = numpy.memmap(memmap_path, dtype=numpy.float32, mode='w+')
-
 raster_path = f'{os.environ["SCRATCH"]}/NCI-WQ-full-2023-04-11-rev443-047031a/NCI-NDRplus-rev443-047031a-intensification/compressed_intensification_300.0_D8_export.tif'
+raster_info = pygeoprocessing.get_raster_info(raster_path)
+memmap_path = f'{os.environ["SCRATCH"]}/memmap.npym'
+memmapped_array = numpy.memmap(
+    memmap_path, dtype=numpy.float32, mode='w+', shape=(
+        raster_info['raster_size'][1], raster_info['raster_size'][0]))
+
 raster = gdal.OpenEx(raster_path)
 band = raster.GetRasterBand(1)
 array = band.ReadAsArray(buf_obj=memmapped_array)
