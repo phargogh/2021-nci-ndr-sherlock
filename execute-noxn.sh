de9ad17db910ffa52e7743304aa4fc810380295c
James Douglass <jdouglass@stanford.edu>
2023-05-19 10:55:46 -0700

    Adding the hostname when we're running on sherlock.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -48,7 +48,7 @@ DIGEST=sha256:2a92ced1387bbfe580065ef98a61f6d360daf90f3afa54cf4383b0becf7480e8
 # NOTE: This repo is private and so requires that sherlock is configured for SSH access.
 REPOSLUG=nci-noxn-levels
 REPO=git@github.com:natcap/$REPOSLUG.git
-REVISION=9ba0fce9ecbd8b1f4817587910441b1d856f9072
+REVISION=4b579aaf3bc2ed64e14c4879da68d321a7d8fd97
 if [ ! -d $REPOSLUG ]
 then
     git clone $REPO
