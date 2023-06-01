227016f5c894956e097f2304c8eba6893cc05282
James Douglass <jdouglass@stanford.edu>
2023-06-01 12:02:29 -0700

    Fiddling with requirements now that prediction is factored out.

--- execute-noxn.sh
+++ execute-noxn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-#SBATCH --time=4:00:00
+#SBATCH --time=2:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jdouglass@stanford.edu
