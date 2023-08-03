6a43ee6815c3d7dbeca89a9181738421a5a2130b
James Douglass <jdouglass@stanford.edu>
2023-08-03 09:54:38 -0700

    Moving intensification n_app op to top level for profiling.

--- prep-ndr-inputs-pipeline.py
+++ prep-ndr-inputs-pipeline.py
@@ -136,32 +136,37 @@ def bmp_op(lu, rb, pv):
     return result
 
 
+def _intensification_n_app(lulc, current, intensification_raw, scenario_nodata,
+                           current_n_app_nodata,
+                           intensification_raw_n_app_nodata):
+    result = numpy.full(lulc.shape, N_APP_NODATA, dtype=numpy.float32)
+    valid_pixels = (
+        ~_equals_nodata(lulc, scenario_nodata) &
+        ~_equals_nodata(current, current_n_app_nodata) &
+        ~_equals_nodata(intensification_raw,
+                        intensification_raw_n_app_nodata))
+
+    # if non-ag, use given current (should be current n_app)
+    result[valid_pixels] = current[valid_pixels]
+
+    # if ag, use intensification_raw n_app
+    ag = (numpy.isin(lulc, AG_LUCODES) & valid_pixels)
+    result[ag] = numpy.maximum(intensification_raw[ag], current[ag])
+
+    return result
+
+
 def intensification_n_app(scenario_lulc, current_n_app,
                           intensification_raw_n_app, target_n_app):
     scenario_nodata = _get_nodata(scenario_lulc)
     current_n_app_nodata = _get_nodata(current_n_app)
     intensification_raw_n_app_nodata = _get_nodata(intensification_raw_n_app)
 
-    def _intensification_n_app(lulc, current, intensification_raw):
-        result = numpy.full(lulc.shape, N_APP_NODATA, dtype=numpy.float32)
-        valid_pixels = (
-            ~_equals_nodata(lulc, scenario_nodata) &
-            ~_equals_nodata(current, current_n_app_nodata) &
-            ~_equals_nodata(intensification_raw,
-                            intensification_raw_n_app_nodata))
-
-        # if non-ag, use given current (should be current n_app)
-        result[valid_pixels] = current[valid_pixels]
-
-        # if ag, use intensification_raw n_app
-        ag = (numpy.isin(lulc, AG_LUCODES) & valid_pixels)
-        result[ag] = numpy.maximum(intensification_raw[ag], current[ag])
-
-        return result
-
     pygeoprocessing.raster_calculator(
         [(scenario_lulc, 1), (current_n_app, 1),
-         (intensification_raw_n_app, 1)],
+         (intensification_raw_n_app, 1), (scenario_nodata, 'raw'),
+         (current_n_app_nodata, 'raw'),
+         (intensification_raw_n_app_nodata, 'raw')],
         _intensification_n_app, target_n_app, N_APP_DTYPE, N_APP_NODATA)
 
 
