7aacc3555214f9b3076779578c17133a820f0486
James Douglass <jdouglass@stanford.edu>
2023-08-15 12:03:00 -0700

    Adding slurm script for model analysis execution.

---
+++ execute-model-analysis.sh
@@ -0,0 +1,41 @@
+##!/bin/bash
+#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=16
+#SBATCH --time=2:00:00
+#SBATCH --ntasks=1
+#SBATCH --partition=hns,normal
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="NCI-NOXN-model-analysis"
+#SBATCH --output=/scratch/users/jadoug06/slurm-logfiles/slurm-%j.%x.out
+
+# "${ENVVAR:-DEFAULT}" means use ENVVAR if present, DEFAULT if not.
+MODEL_ANALYSIS_WORKSPACE="${NOXN_WORKSPACE:-$1}"  # final location of pipeline outputs
+NCI_WORKSPACE="${NCI_WORKSPACE:-$2}"
+
+# load configuration for globus, singularity, etc.
+source "env-sherlock.env"
+
+DATE="$(date +%F)"
+GIT_REV="rev$(git rev-parse --short HEAD)"
+
+CONFIG_FILE="pipeline.config-sherlock-$RESOLUTION.json"
+singularity run \
+    "docker://$NOXN_DOCKER_CONTAINER" \
+    python3 model_analysis.py \
+    --n_workers "$SLURM_CPUS_PER_TASK" \
+    "$CONFIG_FILE" \
+    "$MODEL_ANALYSIS_WORKSPACE" \
+    "$NCI_WORKSPACE/ndr-plus-outputs/"
+
+# Upload files to globus
+# Copy geotiffs AND logfiles, if any, to google drive.
+# This will upload to a workspace with the same dirname as $MODEL_ANALYSIS_WORKSPACE.
+module load system py-globus-cli
+ARCHIVE_DIR="$DATE-nci-noxn-model-analysis-$GIT_REV-slurm$SLURM_JOB_ID"
+GDRIVE_DIR="nci-ndr-stanford-gdrive:$(basename $NCI_WORKSPACE)/$ARCHIVE_DIR"
+globus transfer --fail-on-quota-errors --recursive \
+    --label="NCI WQ NOXN Model Analysis $GIT_REV" \
+    "$GLOBUS_SHERLOCK_SCRATCH_ENDPOINT_ID:$MODEL_ANALYSIS_WORKSPACE" \
+    "$GLOBUS_STANFORD_GDRIVE_COLLECTION_ID:$GLOBUS_STANFORD_GDRIVE_RUN_ARCHIVE/$(basename $NCI_WORKSPACE)/$ARCHIVE_DIR" || echo "Globus transfer failed!"
+
+echo "Model analysis complete."
