d68aa27f089198f41455db6b9e266c02c87222c4
James Douglass <jdouglass@stanford.edu>
2023-08-27 21:31:09 -0700

    Doubling job time, just in case.
    NOXN failed due to a timeout, so just trying to guard against future
    timeouts.  Maybe a network problem?  Unclear.

--- execute-ndr-scenario-batch.sh
+++ execute-ndr-scenario-batch.sh
@@ -92,10 +92,10 @@ if [ "$1" = "--with-noxn" ]
 then
     if [ "$2" = "1km" ]
     then
-        NOXN_TIME="10:00:00"
+        NOXN_TIME="20:00:00"
         NOXN_SPATIAL_CONFIG_FILE="nci-noxn-levels/pipeline.config-sherlock-1km.json"
     else
-        NOXN_TIME="4:00:00"
+        NOXN_TIME="8:00:00"
         NOXN_SPATIAL_CONFIG_FILE="nci-noxn-levels/pipeline.config-sherlock-10km.json"
     fi
     # --dependency=afterok:<id1>:<id2>... means that if the whole NDR pipeline
