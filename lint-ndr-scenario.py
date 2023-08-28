7a3e5dd2fa8c8e5e1bbbe2892d0f81d12d6a2d0c
James Douglass <jdouglass@stanford.edu>
2023-08-28 14:08:11 -0700

    Updating lint script to also check for all-nodata and all-nan rasters.

--- lint-ndr-scenario.py
+++ lint-ndr-scenario.py
@@ -4,6 +4,9 @@ import os
 import pprint
 import sys
 
+import numpy
+import pygeoprocessing
+
 sys.path.append('ndr_plus_global_pipeline/scenarios')
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
@@ -11,7 +14,9 @@ LOGGER = logging.getLogger(__name__)
 
 def main(scenario_name):
     scenario = importlib.import_module(scenario_name)
-    error = False
+    files_not_found_error = False
+    invalid_files_error = False
+    files_checked = set()
     for ndr_scenario_key, files_dict in scenario.SCENARIOS.items():
         for file_key, ecoshards_key in files_dict.items():
             try:
@@ -20,19 +25,40 @@ def main(scenario_name):
                 LOGGER.error(
                     "Key not found in ECOSHARDS: "
                     f"{ndr_scenario_key}:{file_key} ({ecoshards_key})")
-                error = True
+                files_not_found_error = True
                 continue
 
             if not os.path.exists(file_path):
                 LOGGER.error(
                     "File not found: "
                     f"{ndr_scenario_key}:{file_key}:{file_path}")
-                error = True
+                files_not_found_error = True
 
-    if error:
+            if file_path.endswith('.tif'):
+                n_valid_pixels = 0
+                files_checked.add(file_path)
+                raster_nodata = pygeoprocessing.get_raster_info(
+                    file_path)['nodata'][0]
+                for _, block in pygeoprocessing.iterblocks((file_path, 1)):
+                    valid_mask = (
+                        ~numpy.isclose(block, raster_nodata, equal_nan=True) &
+                        ~numpy.isnan(block))
+                    n_valid_pixels += numpy.count_nonzero(valid_mask)
+                if n_valid_pixels == 0:
+                    LOGGER.error(
+                        f"Raster is fully nodata or nan: \n"
+                        f"    NDR scenario: {ndr_scenario_key}\n"
+                        f"    File key: {file_key}\n"
+                        f"    Filepath: {file_path}")
+                    invalid_files_error = True
+
+    if files_not_found_error:
         LOGGER.info("The current state of ECOSHARDS looks like:\n"
                     f"{pprint.pformat(scenario.ECOSHARDS)}")
         raise AssertionError("One or more files/keys do not work as expected.")
+
+    if invalid_files_error:
+        raise AssertionError("One or more files are 100% nodata or NaN.")
     LOGGER.info("All OK")
 
 
