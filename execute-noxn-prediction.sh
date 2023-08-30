4b796d6688bb5fc7076a2a94ac48ac978052b9b1
James Douglass <jdouglass@stanford.edu>
2023-08-30 11:33:36 -0700

    Upping requested runtime, CPUs, tuning down RAM.

--- execute-noxn-prediction.sh
+++ execute-noxn-prediction.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-#SBATCH --time=4:00:00
+#SBATCH --time=12:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=6G
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
 #SBATCH --partition=hns,normal
 #SBATCH --job-name="NCI-NOXN-prediction"
