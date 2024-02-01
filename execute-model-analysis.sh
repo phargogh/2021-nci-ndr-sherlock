3166aa222ef3772558872fc36ddba5921f9731af
James Douglass <jdouglass@stanford.edu>
2024-02-01 15:58:01 -0800

    Increasing allotted time to 4 hours.

--- execute-model-analysis.sh
+++ execute-model-analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=16
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH --ntasks=1
 #SBATCH --partition=hns,normal
 #SBATCH --mail-type=ALL
