8bb669aebc70ac789f747f2273b4e612485a3951
James Douglass <jdouglass@stanford.edu>
2024-04-22 12:37:21 -0700

    Increasing resources and bumping noxn repo.
    Repo bump is to try to eliminate a FutureWarning.

--- execute-model-analysis.sh
+++ execute-model-analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem-per-cpu=4G
-#SBATCH --cpus-per-task=16
-#SBATCH --time=2:00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --time=6:00:00
 #SBATCH --ntasks=1
 #SBATCH --partition=hns,normal
 #SBATCH --mail-type=ALL
