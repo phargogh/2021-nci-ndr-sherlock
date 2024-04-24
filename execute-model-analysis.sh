b9ccd3f124935a404fedbfecbb050833c05fb1b5
James Douglass <jdouglass@stanford.edu>
2024-04-24 15:25:09 -0700

    Doubling runtime again.

--- execute-model-analysis.sh
+++ execute-model-analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem-per-cpu=4G
 #SBATCH --cpus-per-task=20
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --partition=hns,normal
 #SBATCH --mail-type=ALL
